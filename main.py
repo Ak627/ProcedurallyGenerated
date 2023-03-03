9b2a73eff4979ddc3f23c1cca7506768ef5d962f
Ak627 <91678317+Ak627@users.noreply.github.com>
2023-03-03 07:41:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 
 def direction(x, y):
         dir = random.randrange(0, 4)
-        print(dir)
         if dir == 0 and y != 9:
             y += 1
             return x, y
@@ -18,6 +17,8 @@ def direction(x, y):
         elif dir == 3 and x != 0:
             x -= 1
             return x, y
+        else:
+            return x, y
 
 mapGrid = [[0 for i in range(10)] for j in range(10)]
 xpos = random.randrange(0, 9)
@@ -26,7 +27,6 @@ mapGrid[xpos][ypos] = 1
 ticker = 0
 while ticker != 9:
     xpos, ypos = direction(xpos, ypos)
-    print(xpos, ypos )
     if mapGrid[xpos][ypos] == 0:
         mapGrid[xpos][ypos] = random.randrange(2, 5)
         ticker += 1
